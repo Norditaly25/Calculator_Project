64b2cfc778c3b59c44d27f82cfb73a59a16bde6d
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-03-21 14:34:35 +0100

    Finished with coding the functionality for displaying the numbers on the display above the calc, replacing operators both in cases where the user inputs n1, operator, n2, or n1, op, op, or n1, op, n2, op

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-let n1;
-let operator;
-let n2;
+let n1 = "";
+let operator = "";
+let n2 = "";
+let shouldClearDisplay = false;
 
 const add = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
@@ -21,3 +22,41 @@ let operate = function (n1, operator, n2) {
             return "ERROR";
     }
 }
+const display = document.querySelector(".calculator-display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        const digit = number.textContent;
+        display.textContent += digit;
+        operator === "" ? n1 += digit : n2 += digit;
+        });
+    });
+
+    operators.forEach(opBtn => {
+        opBtn.addEventListener("click", () => {
+        const selectedOperator = opBtn.textContent;
+
+        if (operator !== "" && n2 !== "") {
+            const result = operate(Number(n1), operator, Number(n2));
+            display.textContent = result + selectedOperator;
+            n1 = result.toString();
+            operator = selectedOperator;
+            n2 = "";
+        } else if (operator !== "" && n2 === "") {
+            display.textContent = display.textContent.slice(0, -1) + selectedOperator;
+            operator = selectedOperator; 
+        } else {
+            display.textContent += selectedOperator;
+            operator = selectedOperator; 
+        }
+        });
+    });
+
+equals.addEventListener("click", () => {
+    
+    }
+)
+
